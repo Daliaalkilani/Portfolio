880e9e40a35af76a541efea18e95fe49ba64a635
Dalia Al-Kilani <165378708+Daliaalkilani@users.noreply.github.com>
2026-06-24 12:48:31 +0300

    Delete index.ts

--- index.ts
+++
@@ -1,8 +0,0 @@
-export { default as NeuralNetwork } from "./NeuralNetwork";
-export { default as ParticleSystem } from "./ParticleSystem";
-export { default as Terminal } from "./Terminal";
-export { default as NLPDemo } from "./NLPDemo";
-export { default as GlitchText } from "./GlitchText";
-export { default as Reveal } from "./Reveal";
-export { default as MagneticCursor } from "./MagneticCursor";
-export { default as ScrollProgress } from "./ScrollProgress";
